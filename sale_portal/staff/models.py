9a0c7413b4d0f06f5bfc65bfb3c99275c4d055ef
Long Dao <long.daohai4894@gmail.com>
2019-11-13 10:18:49 +0700

    add relation staff_team and save log when staff change team or change role

--- sale_portal/staff/models.py
+++ sale_portal/staff/models.py
@@ -1,24 +1,40 @@
+import logging
+
 from django.db import models
+from django.contrib.auth.models import Group
 from django.contrib.postgres.fields import JSONField
 
+from sale_portal.team.models import Team
 from sale_portal.staff import StaffLogType
 
 
-class QrStaff(models.Model):
-    staff_id=models.IntegerField(primary_key=True, unique=True)
-    staff_code=models.TextField(null=True)
-    nick_name=models.TextField(null=True)
-    full_name=models.TextField(null=True)
-    email=models.TextField(null=True)
-    mobile=models.TextField(null=True)
-    department_code=models.TextField(null=True)
-    status=models.IntegerField(default=1,null=True)
-    created_date=models.DateTimeField(null=True)
-    modify_date=models.DateTimeField(null=True)
-    department_id=models.IntegerField(null=True)
+class StaffTeamRole(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    group = models.OneToOneField(Group, on_delete=models.SET_NULL, blank=True, null=True)
 
     class Meta:
-        db_table='qr_staff'
+        db_table = 'staff_team_role'
+        default_permissions = ()
+
+    def __str__(self):
+        return self.code
+
+
+class QrStaff(models.Model):
+    staff_id = models.IntegerField(primary_key=True, unique=True)
+    staff_code = models.TextField(null=True)
+    nick_name = models.TextField(null=True)
+    full_name = models.TextField(null=True)
+    email = models.TextField(null=True)
+    mobile = models.TextField(null=True)
+    department_code = models.TextField(null=True)
+    status = models.IntegerField(default=1, null=True)
+    created_date = models.DateTimeField(null=True)
+    modify_date = models.DateTimeField(null=True)
+    department_id = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'qr_staff'
         default_permissions = ()
 
     def __str__(self):
@@ -36,6 +52,8 @@ class Staff(models.Model):
     department_id = models.IntegerField(null=True, help_text='Equivalent with qr_staff.department_id')
     created_date = models.DateTimeField(null=True, help_text='Equivalent with qr_staff.created_date')
     modify_date = models.DateTimeField(null=True, help_text='Equivalent with qr_staff.modify_date')
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True)
+    role = models.ForeignKey(StaffTeamRole, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         db_table = 'staff'
@@ -43,6 +61,34 @@ class Staff(models.Model):
 
     def __str__(self):
         return self.email
+
+    def __init__(self, *args, **kwargs):
+        super(Staff, self).__init__(*args, **kwargs)
+        self.__important_fields = ['team_id', 'role_id']
+        for field in self.__important_fields:
+            setattr(self, '__original_%s' % field, getattr(self, field))
+
+    def compare(self):
+        old_data = {}
+        new_data = {}
+        type = StaffLogType.CREATE_NEW
+
+        for field in self.__important_fields:
+            orig = '__original_%s' % field
+            old_data[field] = getattr(self, field)
+            new_data[field] = getattr(self, orig)
+            if getattr(self, orig) != getattr(self, field):
+                type = StaffLogType.UPDATED
+        return old_data, new_data, type
+
+    def save(self, *args, **kwargs):
+        old_data, new_data, type = self.compare()
+        if type == StaffLogType.UPDATED:
+            try:
+                StaffLog.objects.create(old_data=old_data, new_data=new_data, staff_id=self.id, type=type)
+            except Exception as e:
+                logging.error('Staff_log exception: %s', e)
+        super(Staff, self).save(*args, **kwargs)
 
 
 class StaffLog(models.Model):
