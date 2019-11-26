52c3a168930f8aa9d3fafa2baa4ea4c17c50c5c3
Nguyen Dung <dungntc108@gmail.com>
2019-11-26 18:22:43 +0700

    add code

--- sale_portal/shop/views.py
+++ sale_portal/shop/views.py
@@ -1,35 +1,36 @@
 from django.contrib.auth.decorators import login_required, permission_required
-from django.contrib.postgres.search import SearchVector, SearchQuery
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
-from django.db.models import F, Subquery
+from django.db.models import F, Subquery, Q
+from unidecode import unidecode
 
-from sale_portal.shop.models import Shop,vn_unaccent
+from sale_portal.shop.models import Shop, vn_unaccent
 
 
 @api_view(['GET'])
 @login_required
 def list_shop_for_search(request):
-    shop = Shop(
-        merchant_id=5,
-        staff_id=1,
-        name='conditional_escape(name)',
-        code=1,
-        address='test address',
-        province_id=1,
-        district_id=1,
-        wards_id=1,
-        street='test strees',
-        description='conditional_escape(description)',
-        created_by=request.user
-    )
-    shop.save()
-    # Shop.objects.filter(pk=shop.id).update(
-    #     document=SearchVector(vn_unaccent('address'), weight='B') + SearchVector('code', weight='C') + SearchVector(
-    #         Subquery(Shop.objects.filter(pk=shop.id).values('merchant__merchant_brand')[:1]), weight='C')
-    # )
+    name = request.GET.get('name', None)
+    queryset = Shop.objects.all()
+    if name is not None and name != '':
+        name_en = unidecode(name).lower()
+
+        search_query = SearchQuery(name_en)
+        queryset = queryset.filter(
+            Q(document=search_query) | Q(code__icontains=name) | Q(merchant__merchant_brand__icontains=name))
+
+        queryset = queryset.annotate(
+            rank=SearchRank(F('document'), search_query)
+        ).order_by(
+            '-rank'
+        )
+
+    queryset = queryset[0:10]
+    data = [
+        {'address': shop.address}
+        for shop in queryset]
     return JsonResponse({
-        'status': 'SUCCESS',
-        'data': shop.address,
+        'data': data
     }, status=200)
