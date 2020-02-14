91ee73aab726d3c2e02285c768d1aea7f7b8c5b5
unknown <binhdh@vnpay.vn>
2020-02-14 14:19:50 +0700

    add model

--- sale_portal/user/__init__.py
+++ sale_portal/user/__init__.py
@@ -1,6 +1,6 @@
-model_names = ['area', 'cronjoblog', 'customgroup', 'merchant', 'merchantlog', 'permission', 'salepromotion',
-               'salepromotiontitle', 'salereport', 'shop', 'shopcube', 'shoplog', 'staff', 'stafflog', 'staffteamlog',
-               'team', 'teamlog', 'terminal', 'terminallog', 'user']
+model_names = ['area', 'proportion_kpi_team', 'cronjoblog', 'pos365', 'customgroup', 'merchant', 'merchantlog', 'permission', 'salepromotion',
+               'salepromotiontitle', 'salereport', 'shop', 'shopcube', 'shoplog', 'staff', 'stafflog', 'team',
+               'teamlog', 'terminal', 'terminallog', 'user', 'staff_care', 'staff_care_log', 'staff_care_import_log']
 
 # Role user
 ROLE_ADMIN = 'ADMIN'
