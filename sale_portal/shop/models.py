d7825a7c0086fc5239abd516df98d8e7a46f81e8
Nguyen Dung <dungntc108@gmail.com>
2020-03-12 16:22:29 +0700

    fix filter provinces and shop per

--- sale_portal/shop/models.py
+++ sale_portal/shop/models.py
@@ -65,7 +65,8 @@ class Shop(models.Model):
             ('shop_import', 'Can import data shop'),
             ('shop_export', 'Can export data shop'),
             ('dashboard_shop_count', 'Can get shop data for dashboard'),
-            ('shop_assign', 'Can create shop from terminal or assign terminal to shop'),
+            ('shop_assign', 'Can assign terminal to shop'),
+            ('shop_create_from_ter', 'Can create shop from terminal'),
             ('filter_by_cross_assign_status', 'Can filter by cross_assign_status')
         )
 
