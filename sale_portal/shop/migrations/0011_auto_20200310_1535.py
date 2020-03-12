385101d59552b81995c58045cb4063a1630c56bc
Nguyen Dung <dungntc108@gmail.com>
2020-03-12 16:32:51 +0700

    fix filter provinces and shop per

--- sale_portal/shop/migrations/0011_auto_20200310_1535.py
+++ sale_portal/shop/migrations/0011_auto_20200310_1535.py
@@ -12,6 +12,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterModelOptions(
             name='shop',
-            options={'default_permissions': (), 'ordering': ('-created_date',), 'permissions': (('shop_list_data', 'Can get shop list data'), ('shop_detail', 'Can get shop detail'), ('shop_create', 'Can create shop'), ('shop_edit', 'Can edit shop'), ('shop_import', 'Can import data shop'), ('shop_export', 'Can export data shop'), ('dashboard_shop_count', 'Can get shop data for dashboard'), ('shop_assign', 'Can create shop from terminal or assign terminal to shop'), ('filter_by_cross_assign_status', 'Can filter by cross_assign_status'))},
+            options={'default_permissions': (), 'ordering': ('-created_date',), 'permissions': (('shop_list_data', 'Can get shop list data'), ('shop_detail', 'Can get shop detail'), ('shop_create', 'Can create shop'), ('shop_edit', 'Can edit shop'), ('shop_import', 'Can import data shop'), ('shop_export', 'Can export data shop'), ('dashboard_shop_count', 'Can get shop data for dashboard'), ('shop_assign', 'Can assign terminal to shop'), ('filter_by_cross_assign_status', 'Can filter by cross_assign_status'))},
         ),
     ]
