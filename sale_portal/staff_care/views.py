8490e68c57a16ccde6600038df9687a7799f0688
Nguyễn Thế Chí Dũng <dungntc108@gmail.com>
2020-04-07 10:34:03 +0700

    hotfix/import-sale-shop

--- sale_portal/staff_care/views.py
+++ sale_portal/staff_care/views.py
@@ -125,35 +125,39 @@ def update_sale_shop(request, data, is_submit=False):
 
     if data['staff_email'] is None or data['staff_email'] == '':
         if is_submit:
-            shop.staff_delete(request=request)
-        else:
-            return 'Thành công'
-
-    staff = Staff.objects.filter(email=data['staff_email']).first()
-    if staff is None:
-        return 'Sale_email: Không tìm thấy Sale - Lỗi dữ liệu'
-    if staff.team is None:
-        return 'Sale không thuộc bất kì Team nào - Lỗi dữ liệu'
-
-    if shop.staff == staff and (data['street'] == '' or data['street'] is None):
-        return 'No change'
-    elif shop.staff == staff and (data['street'] != '' and data['street'] is not None):
-        if is_submit:
-            shop.street = data['street']
-            shop.save()
-        return 'Thành công'
-    else:
-        if is_submit:
-            if data['street'] != '':
-                shop.street = data['street']
-                shop.save()
-
             if shop.staff:
                 shop.staff_delete(request=request)
-                shop.staff_create(staff_id=staff.id, request=request)
-            else:
-                shop.staff_create(staff_id=staff.id, request=request)
+            if data['street'] != '' and data['street'] is not None:
+                shop.street = data['street']
+                shop.save()
         return 'Thành công'
+    else:
+        staff = Staff.objects.filter(email=data['staff_email']).first()
+        if staff is None:
+            return 'Sale_email: Không tìm thấy Sale - Lỗi dữ liệu'
+        if staff.team is None:
+            return 'Sale không thuộc bất kì Team nào - Lỗi dữ liệu'
+
+        if shop.staff == staff and (data['street'] == '' or data['street'] is None):
+            return 'No change'
+        elif shop.staff == staff and (data['street'] != '' and data['street'] is not None):
+            if is_submit:
+                shop.street = data['street']
+                shop.save()
+            return 'Thành công'
+        else:
+            if is_submit:
+                if data['street'] != '':
+                    shop.street = data['street']
+                    shop.save()
+
+                if shop.staff:
+                    shop.staff_delete(request=request)
+                    shop.staff_create(staff_id=staff.id, request=request)
+                else:
+                    shop.staff_create(staff_id=staff.id, request=request)
+            return 'Thành công'
+
 
 
 def sale_shop_render_excel_import_error(username='', data=[]):
